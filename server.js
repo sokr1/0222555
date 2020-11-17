ad16ea813117a630a6cfbb35218b006bd30a2ef3
sokr1 <67761831+sokr1@users.noreply.github.com>
2020-11-17 02:58:50 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const moment = require("moment");
+var Jimp = require("jimp");
+const { Client, Util } = require("discord.js");
+const fs = require("fs");
+const prefix = ayarlar.prefix;//1  
+const db = require("quick.db");
+require("./util/eventLoader.js")(client);
+const request = require("request");
+const snekfetch = require("snekfetch");
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//-------------------------------OTOROL---------------------------------\\
+*/
